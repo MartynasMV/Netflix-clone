6e84de646be23f132ba01d9c9a382196bd650f5b
Martynas Voldemaras <42231878+MartynasMV@users.noreply.github.com>
2021-03-10 12:14:48 +0100

    final

--- js/main.js
+++ js/main.js
@@ -1,26 +1,25 @@
-const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab-content-item');
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
 
 //select tab content item
 function selectItem(e) {
-    removeBorder();
-    removeShow();
-    //add border to current tab
-    this.classList.add('tab-border');
-    //grab content item from DOM:
-    const tabContentItem = document.querySelector(`#${this.id}-content`);
-    //add show class
-    tabContentItem.classList.add('show');
+  removeBorder();
+  removeShow();
+  //add border to current tab
+  this.classList.add("tab-border");
+  //grab content item from DOM:
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  //add show class
+  tabContentItem.classList.add("show");
 }
 
 function removeBorder(e) {
-    tabItems.forEach(item => item.classList.remove('tab-border'))
-
+  tabItems.forEach((item) => item.classList.remove("tab-border"));
 }
 
 function removeShow(e) {
-    tabContentItems.forEach(item => item.classList.remove('show'))
+  tabContentItems.forEach((item) => item.classList.remove("show"));
 }
 
 //listen for tab click
-tabItems.forEach(item => item.addEventListener('click', selectItem))
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
